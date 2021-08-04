00b76e95426191924e56c6aab4a386d0d3be3fcc
SIA <52838444+CODEORDIETRYING@users.noreply.github.com>
2021-08-04 16:49:52 +0100

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [id]
-      ,[wage]
-  FROM [DataCleaningPractice ].[dbo].[new_employees]
-
-select GREATEST (17, wage) as wage
-from new_employees
-
